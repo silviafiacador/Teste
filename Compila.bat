ddaa2f53e2eecfac9dac98936c0fada320d86221
silviafiacador <silviafiacador1608@gmail.com>
2017-06-21 10:12:17 -0300

    Add files via upload

---
+++ Compila.bat
@@ -0,0 +1,13 @@
+c:
+cd\
+cd \Program Files\Java\jdk1.8.0_111\bin
+javac C:\Users\silvia.scsfiacador\Desktop\java\aula1\OlaMundo.java
+cd C:\Users\silvia.scsfiacador\Desktop\java\aula1
+java OlaMundo
+pause
+
+
+
+
+
+
---
+++ OlaMundo.java
@@ -0,0 +1,19 @@
+// comentário de uma linha
+
+/* comentário 
+em 
+bloco
+*/
+
+public class OlaMundo {
+	//método = algo que exerce uma ação
+	//método main -nomes de métodos: minúsculo
+	public static void main (String args[])  {
+		//esta classe deve imprimir Olá Mundo
+		System.out.println ("Olá Mundo");
+		System.out.println ("Bom dia"); 
+	}
+}
+		
+		
+		
